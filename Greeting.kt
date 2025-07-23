88511a460990ebab39a58d885e96a3b5b7d438ba
Kabi Rengoku <kabi.rengoku2004@gmail.com>
2025-07-23 16:37:46 +0530

    Version 5

--- Greeting.kt
+++ Greeting.kt
@@ -3,14 +3,23 @@ package eu.tutorial.gittutorials
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import eu.tutorial.gittutorials.ui.theme.GitTutorialsTheme
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
+        )
     )
 }
 
